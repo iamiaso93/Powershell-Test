912ab625e5ae7ebae5a6ae9be68ef848fd8190ac
iamiaso93 <75516356+iamiaso93@users.noreply.github.com>
2020-12-05 22:20:45 -0600

    Message

---
+++ JordanModule13.ps1
@@ -0,0 +1,65 @@
+function  Test-CloudFlare {
+    param (
+        [Parameter(ValueFromPipeline=$True,
+            Mandatory=$True) ]
+            [Alias('CN','Name')]
+            [string[]]$Computername,
+        [Parameter(Mandatory=$false)]
+            [ValidateSet('Text','Host','CSV')]
+            [string]$Output = 'Host',
+        [Parameter(Mandatory=$False)]
+            [string] $Env:USERPROFILE
+
+    ) #Param
+    foreach ($Computer in $Computername) {
+        $DateTime = Get-Date
+        $TestCF = Test-NetConnection -ComputerName one.one.one.one -InformationLevel Detailed
+    try {
+        $Params = @{
+            'ComputerName'=$Computer
+            'ErrorAction'='Stop'
+        }
+        $Session = New-PSSession -ComputerName @Params
+        Enter-PSSession $Session
+        Exit-PSSession
+        Remove-PSSession -Session $Session
+       $OBJ = [PSCustomObject]@{
+        'Computername' = $Computername
+        'PingSuccess' = $TestCF.TcpTestSucceededcls
+        'NameResolve' = $TestCF.NameResolutionSucceded
+        'ResolvedAddresses' = $TestCF.ResolvedAddresses
+        }
+
+    }#try
+    catch {
+        Write-Host "Remote connection to $Computername failed." -ForegroundColor Red
+    }#Catch
+
+}
+#Foreach
+switch ($Output) {
+    "Host" {  
+        $OBJ }
+    
+    "Text" {
+    
+        #Receives the job results
+        Write-Verbose "Receiving test results"
+        $OBJ| Out-File .\JobResults.txt
+    
+    
+    
+    
+        Write-Verbose "Generating results file"
+        Add-Content -Path "$Path.\RemTestNet.txt" -Value "Computer Tested: $Computername"
+        Add-Content -Path "$Path.\RemTestNet.txt" -Value "Date/Time Tested: $DateTime"
+        Add-Content -Path "$Path.\RemTestNet.txt" -Value (Get-Content -Path .\JobResults.txt)
+        notepad "$Path.\RemTestNet.txt"
+        Remove-Item .\JobResults.txt
+    }
+    "CSV" {
+        Write-Verbose "Opening results"
+        $OBJ | Export-Csv -Path .\TestResults.csv
+    }
+    }#Switch
+}#Function
